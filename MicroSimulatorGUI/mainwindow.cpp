9123855246c261ba9d01d0322d21b6abb9ed051e
phillips1166 <phillips.1166@osu.edu>
2016-01-24 13:40:05 -0500

    Fully Functional Editor Tab

--- MicroSimulatorGUI/mainwindow.cpp
+++ MicroSimulatorGUI/mainwindow.cpp
@@ -1,5 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QPlainTextEdit>
+#include <QTextStream>
+#include <QFile>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +15,39 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_Load_Text_File_released()
+{
+    QString inputfilename = QFileDialog::getOpenFileName(this, tr("Open Text File"), "", tr("Text files (*.txt)"));
+    QFile inputfile(inputfilename);
+    inputfile.open(QFile::ReadOnly | QFile::Text);
+    ui->Editor_Window->setPlainText(inputfile.readAll());
+    inputfile.close();
+}
+
+void MainWindow::on_Save_Text_File_released()
+{
+    QString outputfilename = QFileDialog::getSaveFileName(this, tr("Save File"), "", tr("Text files (*.txt)"));
+    QFile outputfile(outputfilename);
+    outputfile.open(QFile::WriteOnly | QFile::Text);
+    QTextStream out(&outputfile);
+
+    //outputfile.write(ui->Editor_Window->toPlainText());
+
+    out << ui->Editor_Window->toPlainText();
+
+
+    outputfile.close();
+
+}
+
+void MainWindow::on_Clear_released()
+{
+    ui->Editor_Window->clear();
+}
+
+void MainWindow::on_Add_to_Debug_clicked()
+{
+    ui->AssemblyCode->setPlainText(ui->Editor_Window->toPlainText());
 }
--- MicroSimulatorGUI/mainwindow.h
+++ MicroSimulatorGUI/mainwindow.h
@@ -15,6 +15,16 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+
+    void on_Load_Text_File_released();
+
+    void on_Save_Text_File_released();
+
+    void on_Clear_released();
+
+    void on_Add_to_Debug_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
