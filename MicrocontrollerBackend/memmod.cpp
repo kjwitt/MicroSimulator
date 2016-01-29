76bff915720bd60f742722e1c3bb2cbd0b04a598
plyelectronics <ply.electronics@gmail.com>
2016-01-29 14:43:38 -0500

    …ray used as Microbaby's memory. Which memory is selected to be modified is based off the pointer passed in the function parameters. Please see the comments in memmod.cpp for parameters. Contact me with any questions.

---
+++ MicrocontrollerBackend/memmod.cpp
@@ -0,0 +1,38 @@
+/*
+ * memmod.cpp
+ *
+ *  Created on: Jan 29, 2016
+ *      Author: Logan Myers
+ *
+ *      This is the function that will change a
+ *      char array used as instruction memory and
+ *      data memory of the Microbaby
+ *
+ *      char memmod(char addrval, char dataval, bool rw, char* memarry)
+ *
+ *      addrval	:char value of address to be accessed
+ *      dataval	:char value of data to put in address (used when rw=0, don't care otherwise)
+ *      rw		: 0 = read
+ *      		: 1 = write
+ *      memarry	: pointer to char array of memory to be changed
+ *
+ *      Example(s)reading: IR = memmod(addr,0,0, &datamemoryarray);
+ *      		  writing: memmod(addr,data,1, &datamemoryarray);
+ */
+#ifndef _MEMMOD_CPP
+#define _MEMMOD_CPP
+
+//Functions
+char memmod(char addrval, char dataval, bool rw, char* memarray){
+	char output = 0;
+
+	if (rw==0){
+		output = *memarray[addrval];
+	} else if (rw==1) {
+		*memarray[addrval] = dataval;
+	}
+
+	return output;
+}
+
+#endif
